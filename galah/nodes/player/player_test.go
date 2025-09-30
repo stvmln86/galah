13bb4d1915f911e17a8ae347ce27301b05ed5eb8
Stephen Malone <mail@wirehaiku.org>
2025-10-01 07:20:07 +1000

    Added nodes/player package.

---
+++ galah/nodes/player/player_test.go
@@ -0,0 +1,32 @@
+package player
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stvmln86/galah/galah/terms/flag"
+)
+
+func TestNew(t *testing.T) {
+	// success
+	plyr := New("@WK")
+	assert.Equal(t, flag.Flag("@WK"), plyr.flag)
+}
+
+func TestFlag(t *testing.T) {
+	// setup
+	plyr := New("@WK")
+
+	// success
+	flag_ := plyr.Flag()
+	assert.Equal(t, flag.Flag("@WK"), flag_)
+}
+
+func TestWall(t *testing.T) {
+	// setup
+	plyr := New("mWK")
+
+	// success
+	okay := plyr.Wall()
+	assert.False(t, okay)
+}
