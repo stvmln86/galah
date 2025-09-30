c3173156853de460f9957364f635e6cfee46e3d0
Stephen Malone <mail@wirehaiku.org>
2025-09-30 15:15:59 +1000

    Added games/node package.

---
+++ galah/games/node/node.go
@@ -0,0 +1,13 @@
+// Package node implements the Node interface and functions.
+package node
+
+import "flag"
+
+// Node is a single abstract entity in the gameworld.
+type Node interface {
+	// Flag returns the Node's drawable Flag.
+	Flag() flag.Flag
+
+	// Wall returns true if the Node cannot be moved through.
+	Wall() bool
+}
