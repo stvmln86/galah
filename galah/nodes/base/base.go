a91cb52ffcde61e6b6037bb2392299bb01c0745c
Stephen Malone <mail@wirehaiku.org>
2025-10-01 21:08:46 +1000

    Added nodes/base package.

---
+++ galah/nodes/base/base.go
@@ -0,0 +1,34 @@
+// Package base implements the BaseNode type and methods.
+package base
+
+import (
+	"github.com/stvmln86/galah/galah/terms/cell"
+)
+
+// BaseNode is a basic Node implementation.
+type BaseNode struct {
+	cell *cell.Cell
+	name string
+	open bool
+}
+
+// New returns a new BaseNode.
+func New(rune, tone rune, name string, open bool) *BaseNode {
+	cell := cell.New(rune, tone)
+	return &BaseNode{cell, name, open}
+}
+
+// Cell returns the BaseNode's Cell.
+func (n *BaseNode) Cell() *cell.Cell {
+	return n.cell
+}
+
+// Name returns the BaseNode's name.
+func (n *BaseNode) Name() string {
+	return n.name
+}
+
+// Open returns true if the BaseNode can be moved through.
+func (n *BaseNode) Open() bool {
+	return n.open
+}
---
+++ galah/nodes/base/base_test.go
@@ -0,0 +1,39 @@
+package base
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stvmln86/galah/galah/terms/cell"
+)
+
+var (
+	mockCell = cell.New('A', 'B')
+	mockNode = New('A', 'B', "test", true)
+)
+
+func TestNew(t *testing.T) {
+	// success
+	node := New('A', 'B', "test", true)
+	assert.Equal(t, mockCell, node.cell)
+	assert.Equal(t, "test", node.name)
+	assert.Equal(t, true, node.open)
+}
+
+func TestCell(t *testing.T) {
+	// success
+	cell := mockNode.Cell()
+	assert.Equal(t, mockCell, cell)
+}
+
+func TestName(t *testing.T) {
+	// success
+	name := mockNode.Name()
+	assert.Equal(t, "test", name)
+}
+
+func TestOpen(t *testing.T) {
+	// success
+	open := mockNode.Open()
+	assert.Equal(t, true, open)
+}
