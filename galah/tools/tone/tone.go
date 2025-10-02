a2567b575de7bed731ba0b92d571653569cc0ae2
Stephen Malone <mail@wirehaiku.org>
2025-10-02 20:07:55 +1000

    Added tools/tone package.

---
+++ galah/tools/tone/tone.go
@@ -0,0 +1,57 @@
+// Package tone implements terminal colour constants.
+package tone
+
+import (
+	"github.com/gdamore/tcell/v2"
+)
+
+// Default is the standard default terminal colour.
+const Default = tcell.ColorDefault
+
+// Aqua is the standard aqua terminal colour.
+const Aqua = tcell.ColorAqua
+
+// Black is the standard black terminal colour.
+const Black = tcell.ColorBlack
+
+// Blue is the standard blue terminal colour.
+const Blue = tcell.ColorBlue
+
+// Fuchsia is the standard fuchsia terminal colour.
+const Fuchsia = tcell.ColorFuchsia
+
+// Grey is the standard grey terminal colour.
+const Grey = tcell.ColorGray
+
+// Green is the standard green terminal colour.
+const Green = tcell.ColorGreen
+
+// Lime is the standard lime terminal colour.
+const Lime = tcell.ColorLime
+
+// Maroon is the standard maroon terminal colour.
+const Maroon = tcell.ColorMaroon
+
+// Navy is the standard navy terminal colour.
+const Navy = tcell.ColorNavy
+
+// Olive is the standard olive terminal colour.
+const Olive = tcell.ColorOlive
+
+// Purple is the standard purple terminal colour.
+const Purple = tcell.ColorPurple
+
+// Red is the standard red terminal colour.
+const Red = tcell.ColorRed
+
+// Silver is the standard silver terminal colour.
+const Silver = tcell.ColorSilver
+
+// Teal is the standard teal terminal colour.
+const Teal = tcell.ColorTeal
+
+// White is the standard white terminal colour.
+const White = tcell.ColorWhite
+
+// Yellow is the standard yellow terminal colour.
+const Yellow = tcell.ColorYellow
